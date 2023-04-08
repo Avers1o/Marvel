86d1789bb557b4c312be821bc662ac565d42efa7
Avers1o <atlant1696@mail.ru>
2023-04-08 03:15:25 +0300

    Add PropTypes

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -100,6 +101,10 @@ const CharListItem = ({ name, thumbnail, onCharSelected }) => {
       <div className="char__name">{name}</div>
     </li>
   );
+};
+
+CharList.propTypes = {
+  onCharSelected: PropTypes.func,
 };
 
 export default CharList;
