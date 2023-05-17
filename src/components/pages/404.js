62e741642ba8f08edb8efacb7b2d9d42883368c8
Avers1o <atlant1696@mail.ru>
2023-05-17 21:48:43 +0300

    Use react-helmet

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,15 @@
+import { Helmet } from "react-helmet";
+
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
   return (
     <div>
+      <Helmet>
+        <meta name="description" content="404 page" />
+        <title>404 page</title>
+      </Helmet>
       <ErrorMessage />
       <p style={{ textAlign: "center", fontWeight: "bold", fontSize: "24px" }}>Page doesn"t exist</p>
       <Link style={{ display: "block", textAlign: "center", fontWeignt: "bold", fontSize: "24px", marginTop: "30px" }} to="/">
